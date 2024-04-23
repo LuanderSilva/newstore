fc0984152787099215724a036c601fffdb61eca5
Luander Silva <luanderfrota22@gmail.com>
2024-04-23 06:18:38 -0300

    :lipstick: feat: Estilização do componente Carrossel

--- src/componentes/SectionCarrossel/styles.jsx
+++ src/componentes/SectionCarrossel/styles.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const Section = styled.section`
-    margin-top: 97px;
+    margin-top: 100px;
 
     .s1-title {
         color: #fff;
@@ -9,5 +9,22 @@ export const Section = styled.section`
         margin-bottom: 35px;
         font-size: 2rem;
         letter-spacing: 1px;
+    }
+
+     .slick-slide {
+        padding-right: 10px;
+    }
+
+    .slick-next {
+       width: 25px;
+    }
+
+    .slick-prev:before {
+        font-size: 36px;
+        margin-left: -20px;
+    }
+
+    .slick-next:before {
+        font-size: 36px;
     }
 `
