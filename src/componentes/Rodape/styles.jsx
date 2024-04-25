f1f0d134d67d0410dbfcf99ab90dfc6902a5acf8
Luander Silva <luanderfrota22@gmail.com>
2024-04-25 11:36:40 -0300

    :lipstick: feat: Estilização do componente Rodape

---
+++ src/componentes/Rodape/styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+    display: flex;
+    justify-content: center;
+    background-color: #1E1E1E;
+    color: #FFF;
+    padding: 15px;
+    margin-top: 15px;
+`
