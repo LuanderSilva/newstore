c4ce4d535d826c3928476310a0866d9ce6631f70
Luander Silva <luanderfrota22@gmail.com>
2024-04-25 09:29:36 -0300

    :card_file_box: raw: Dados dos produtos

---
+++ src/data/data.jsx
@@ -0,0 +1,75 @@
+export const productCarouselData = [
+    {
+        img: "./img-produtos/teclado-t-dagger.png",
+        alt: "Teclado T-Dagger",
+        description: "Teclado Gamer Mecânico T-Dagger Bora, Switch Brown, RGB, Black, ABNT2",
+        price: "165,99",
+        discount: "no PIX com 15% de desconto",
+    },
+    {
+        img: "./img-produtos/teclado-redragon.jpg",
+        alt: "Teclado Redragon",
+        description: "Teclado Mecânico Gamer Redragon Kala Lunar RGB, Switch Blue, ABNT-2, White",
+        price: "259,90",
+        discount: "no PIX com 15% de desconto",
+    },
+    {
+        img: "./img-produtos/gabinete-white-Montech.png",
+        alt: "Gabinete Gamer Montech x3 MESH",
+        description: "Gabinete Gamer Montech x3 MESH, Mid Tower, White, ATX, Com 6 Fans Rainbow, Vidro Temperado, X3 MESH",
+        price: "239,90",
+        discount: "12x de R$ 23,52 sem juros",
+    },
+    {
+        img: "./img-produtos/gabinete-gamer-ninja.png",
+        alt: "Gabinete Gamer Ninja Apolo",
+        description: "Gabinete Gamer Ninja Apolo, Mid Tower, RGB, mATX, Black, Sem Fonte, Com 3 Fans",
+        price: "159,90",
+        discount: "9x de R$ 20,90 sem juros",
+    },
+    {
+        img: "./img-produtos/monitor-gamer.jpg",
+        alt: "Monitor Gamer SuperFrame",
+        description: "Monitor Gamer SuperFrame Precision, 27 Pol, Curvo, Full HD, 1ms, 240Hz, FreeSync, HDMI/DP, SFP2702G",
+        price: "999,90",
+        discount: "12x de R$ 98,03 sem juros",
+    },
+    {
+        img: "./img-produtos/mouse-black.jpg",
+        alt: "Mouse Gamer Razer Cobra",
+        description: "Mouse Gamer Razer Cobra, RGB, 6 Botões Programáveis, 8.500 DPI, Black, RZ0104650100R3U",
+        price: "339,90",
+        discount: "12x de R$ 33,32 sem juros",
+    }
+]
+
+export const productsData = [
+    {
+        img: "./img-produtos/placa-de-video.jpg",
+        alt: "Placa de vídeo",
+        description: "Placa de Vídeo Galax, GeForce, GTX 1650 EX PLUS, 4GB, 1-Click OC, GDDR6, 128-bit, 65SQL8DS93E1",
+        price: "849,99",
+        discount: "12x de R$ 83,33 sem juros"
+    },
+    {
+        img: "./img-produtos/gabinete-white-Montech.png",
+        alt: "Gabinete Gamer Montech x3 MESH",
+        description: "Gabinete Gamer Montech x3 MESH, Mid Tower, White, ATX, Com 6 Fans Rainbow, Vidro Temperado, X3 MESH",
+        price: "239,90",
+        discount: "12x de R$ 23,52 sem juros"
+    },
+    {
+        img: "./img-produtos/fone-white.jpg",
+        alt: "Headset Gamer White",
+        description: "Headset Gamer Fifine SuperFrame Edition SFH6, 7.1 Surround, Drivers de 50mm, RGB, USB, White",
+        price: "159,90",
+        discount: "9x de R$ 20,90 sem juros"
+    },
+    {
+        img: "./img-produtos/fone-black.jpg",
+        alt: "Headset Gamer Black",
+        description: "Headset Gamer SuperFrame, VELKA, 7.1 Surround, RGB, USB, Black, Drivers de 50mm",
+        price: "112,99",
+        discount: "6x de $ 22,16 sem juros"
+    }
+]
