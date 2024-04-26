bd3d7264b1d4a3b66efcf4176b749a3a569d4d41
Luander Silva <luanderfrota22@gmail.com>
2024-04-26 11:07:34 -0300

    :sparkles: feat: Adicionando o título da aba no browser

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,16 @@ import Header from "./componentes/Header/header";
 import SectionCarrossel from "./componentes/SectionCarrossel/sectioncarrossel";
 import MaisVendidos from "./componentes/MaisVendidos/maisvendidos";
 import Rodape from "./componentes/Rodape/rodape";
+import { useEffect } from "react";
 
-function Home() {   
+function Home({ title }) { 
+
+    // Titulo da aba
+
+    useEffect(() => {
+        document.title = title
+    }, [title])
+
     return (
         <>
             <Header />
