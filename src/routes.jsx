8879d2e518cba8a06293d1678469582b2f0833dc
Luander Silva <luanderfrota22@gmail.com>
2024-04-30 11:40:16 -0300

    :sparkles: feat: Importando o CartFavoritesProvider

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,23 +2,26 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './Home.jsx'
 import Carrinho from './pages/Carrinho/Carrinho'
 import Favoritos from './pages/Favoritos/favoritos'
+import { CartFavoritesProvider } from './hooks/useCartFavoritesContext';
 
 function AppRoutes() {
     return (
-        <Routes>
-            <Route
-                path='/'
-                element={<Home title="New Store - A loja do futuro" />}
-            />
-            <Route
-                path='/Carrinho'
-                element={<Carrinho title="New Store - Carrinho" />}
-            />
-             <Route
-                path='/Favoritos'
-                element={<Favoritos title="New Store - Favoritos" />}
-            />
-        </Routes>
+        <CartFavoritesProvider>
+            <Routes>
+                <Route
+                    path='/'
+                    element={<Home title="New Store - A loja do futuro" />}
+                />
+                <Route
+                    path='/Carrinho'
+                    element={<Carrinho title="New Store - Carrinho" />}
+                />
+                <Route
+                    path='/Favoritos'
+                    element={<Favoritos title="New Store - Favoritos" />}
+                />
+            </Routes>
+        </CartFavoritesProvider>
     )
 }
 
