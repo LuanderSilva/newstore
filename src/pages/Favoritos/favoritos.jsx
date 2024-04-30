6751a21740ec0c4f3fc06671c0aab3c6ec458501
Luander Silva <luanderfrota22@gmail.com>
2024-04-30 11:54:04 -0300

    :sparkles: feat: Sistema de produtos adicionados aos favoritos

--- src/pages/Favoritos/favoritos.jsx
+++ src/pages/Favoritos/favoritos.jsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react"
-import Header from '../../componentes/Header/header'
-import Footer from "../../componentes/Rodape/rodape"
 import { Link } from "react-router-dom"
 import { Menu, Container, ContentCarrinho, Produtos, ContainerFooter } from './styles'
+import { useCartFavoritesContext } from "../../hooks/useCartFavoritesContext"
+import Header from '../../componentes/Header/header'
+import Footer from "../../componentes/Rodape/rodape"
+import Cards from "../../componentes/Cards/cards"
+
 
 
 function Favoritos({ title }) {
+    const { favoriteItems } = useCartFavoritesContext()
 
     // Titulo da aba
 
@@ -34,7 +38,23 @@ function Favoritos({ title }) {
                         <h1 className="titleh1">Meus favoritos</h1>
 
                         <Produtos>
-                            <span className="span-title">Nenhum produto adicionado.</span>
+                            {favoriteItems.length === 0 ? (
+                                <span className="span-title">Nenhum produto adicionado.</span>
+                            ) : (
+                                <ul className="list-produtos">
+                                    {favoriteItems.map((item, index) => (
+                                        <li key={index}>
+                                            <Cards
+                                                img={item.img}
+                                                alt={item.alt}
+                                                description={item.description}
+                                                price={item.price}
+                                                discount={item.discount}
+                                            />
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
                         </Produtos>
                     </ContentCarrinho>
                 </Container>
