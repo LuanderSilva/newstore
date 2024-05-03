129d8900323a907819fa71dade1355749537c993
Luander Silva <luanderfrota22@gmail.com>
2024-05-03 11:19:53 -0300

    :lipstick: feat: Reajustando estilização

--- src/pages/Carrinho/styles.jsx
+++ src/pages/Carrinho/styles.jsx
@@ -34,16 +34,72 @@ export const ContentCarrinho = styled.div`
 `
 
 export const Produtos = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap: 25px;
+
     .list-produtos {
         display: grid;
         grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
         margin-top: 60px;
         grid-gap: 10px;
         list-style-type: none;
+        width: 100%;
+    }
+
+    .list-items:last-child {
+        margin-bottom: 50px;
     }
 
     .span-title {
         color: #FFF;
+    }
+`
+
+export const FinalizarPedido = styled.div`
+    margin-top: 63px;
+
+    .card-finalizar {
+        background-color: #FFF;
+        padding: 20px;
+        width: 270px;
+        height: 170px;
+        border-radius: 5px;
+    }
+
+    .title-resumo {
+        text-align: center;
+        margin-bottom: 20px;
+        font-weight: 400;
+    }
+
+    .total {
+        display: flex;
+        justify-content: space-between;
+        border-top: 1px solid #E1E1E1;
+    }
+
+    .text,
+    .value {
+        margin-top: 40px;
+        font-size: 1.2rem;
+    }
+
+    .value {
+        color: #007837;
+    }
+
+    .button-finalizar {
+        padding: 15px;
+        width: 100%;
+        border: none;
+        background-color: #2CB67D;
+        border-radius: 5px;
+        margin-top: 5px;
+        color: #FFF;
+        font-family: 'Archivo', sans-serif;
+        letter-spacing: 1px;
+        cursor: pointer;
     }
 `
 
