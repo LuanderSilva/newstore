65ef39daa6dbf1512db60ae119e3069b13398e1d
Luander Silva <luanderfrota22@gmail.com>
2024-05-03 11:17:08 -0300

    :sparkles: feat: Novos recursos no sistema de carrinho

--- src/hooks/useCartFavoritesContext.jsx
+++ src/hooks/useCartFavoritesContext.jsx
@@ -1,23 +1,49 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react'
 
-const CartFavoritesContext = createContext();
+const CartFavoritesContext = createContext()
 
-export const useCartFavoritesContext = () => useContext(CartFavoritesContext);
+export const useCartFavoritesContext = () => useContext(CartFavoritesContext)
 
 export const CartFavoritesProvider = ({ children }) => {
-    const [cartCount, setCartCount] = useState(0);
-    const [favoritesCount, setFavoritesCount] = useState(0);
-    const [cartItems, setCartItems] = useState([]);
-    const [favoriteItems, setFavoriteItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0)
+    const [favoritesCount, setFavoritesCount] = useState(0)
+    const [cartItems, setCartItems] = useState([])
+    const [favoriteItems, setFavoriteItems] = useState([])
 
     const addToCart = (product) => {
-        setCartItems(prevItems => [...prevItems, product]);
-        setCartCount(prevCount => prevCount + 1);
-    }
+        const existingItemIndex = cartItems.findIndex(item => item.alt === product.alt)
+        if (existingItemIndex !== -1) {
+            const updatedCartItems = [...cartItems]
+            updatedCartItems[existingItemIndex].quantity += 1
+            setCartItems(updatedCartItems)
+        } else {
+            setCartItems([...cartItems, {...product, quantity: 1,}])
+        }
 
+        setCartCount(prevCount => prevCount + 1)
+    };
+
+    const removeFromCart = (productAlt, removeAll = false) => {
+        if (removeAll) {
+            const updatedCartItems = cartItems.filter(item => item.alt !== productAlt)
+            const remainingItemsCount = updatedCartItems.reduce((total, item) => total + item.quantity, 0)
+            setCartItems(updatedCartItems)
+            setCartCount(remainingItemsCount)
+        } else {
+            const updatedCartItems = cartItems.map(item => {
+                if (item.alt === productAlt && item.quantity > 1) {
+                    return { ...item, quantity: item.quantity - 1 }
+                }
+                return item
+            })
+            setCartItems(updatedCartItems);
+            setCartCount(prevCount => prevCount - 1)
+        }
+    };
+    
     const addToFavorites = (product) => {
-        setFavoriteItems(prevItems => [...prevItems, product]);
-        setFavoritesCount(prevCount => prevCount + 1);
+        setFavoriteItems(prevItems => [...prevItems, product])
+        setFavoritesCount(prevCount => prevCount + 1)
     }
 
     return (
@@ -28,6 +54,7 @@ export const CartFavoritesProvider = ({ children }) => {
                 cartItems, 
                 favoriteItems,
                 addToCart, 
+                removeFromCart,
                 addToFavorites 
             }}>
             {children}
